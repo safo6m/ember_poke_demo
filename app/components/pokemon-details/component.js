e8b6f0a2a8d32ad0eefc59d731319ab5878e0919
Mihael Safaric <mihael.safaric@gmail.com>
2016-04-26 01:01:41 +0200

    Add action on avatar

--- app/components/pokemon-details/component.js
+++ app/components/pokemon-details/component.js
@@ -5,5 +5,11 @@ export default Ember.Component.extend({
 
   density: Ember.computed('pokemon.height', 'pokemon.weight', function() {
     return this.get('pokemon.height') * this.get('pokemon.weight');
-  })
+  }),
+
+  actions: {
+    onAvatarClick() {
+      alert(`I'm ${this.get('pokemon.name')}`);
+    }
+  }
 });
