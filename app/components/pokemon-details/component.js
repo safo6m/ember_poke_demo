0a4cedc89d54891b8ddf7209eda41f494e4f0988
Mihael Safaric <mihael.safaric@gmail.com>
2016-04-26 00:59:41 +0200

    Add all pokemon-details component

--- app/components/pokemon-details/component.js
+++ app/components/pokemon-details/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['pokemon-details'],
+
+  density: Ember.computed('pokemon.height', 'pokemon.weight', function() {
+    return this.get('pokemon.height') * this.get('pokemon.weight');
+  })
 });
--- app/models/pokemon.js
+++ app/models/pokemon.js
@@ -1,6 +1,14 @@
 import Model from 'ember-data/model';
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  height: DS.attr('number'),
+  weight: DS.attr('number'),
+  sprites: DS.attr(),
+
+  avatarUrl: Ember.computed('sprites', function() {
+    return this.get('sprites.front_shiny');
+  })
 });
